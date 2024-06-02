eb1a144f7a5e998a124914bb748125093165d177
easydigs <easydigitalkoncept@gmail.com>
2024-06-02 17:48:01 +0100

    Task 1

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Flask App that integrates with AirBnB static HTML Template
+script that starts a Flask web application with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
